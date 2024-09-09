fa622810e883faf3d763b47b530e20a50b1c22ca
Negash Israel <negashjsrael@gmail.com>
2024-09-09 20:25:25 +0300

    footer and Header

---
+++ netflix_clone/src/componets/Footer/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer_outer_container">
+      <div className="foote_inner_conatiner">
+        <div className="footer_icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Dsecription</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help center </li>
+              <li>Jobes</li>
+              <li>cookies preferrences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corportate Information </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service_Code">
+          <p> service Code</p>
+        </div>
+        <div className="copy_write">&copy; 1997-2024 Netflix, Inc.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ netflix_clone/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../componets/Header/Header.js";
+import Footer from "../../componets/Footer/Footer.js";
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+export default Home;
