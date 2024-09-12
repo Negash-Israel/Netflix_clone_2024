05311520e79fb4a4c5082cf9813256ee7a963c9b
Negash Israel <negashjsrael@gmail.com>
2024-09-12 19:53:35 +0300

    request and axios

--- netflix_clone/src/pages/Home/Home.js
+++ netflix_clone/src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../../componets/Header/Header.js";
 import Footer from "../../componets/Footer/Footer.js";
+import Banner from "../../componets/Banner/Banner.js";
+import RowList from "../../componets/Rows/RowList.js";
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
+      <RowList />
       <Footer />
     </>
   );
